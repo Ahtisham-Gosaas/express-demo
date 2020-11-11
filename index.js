ba94df98ee41b7be44546c28b0890ac59eacfe52
Ahtisham <ahtisham@gosaas.io>
2020-11-11 16:01:05 +0500

    all application logic without proper application structure

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+const config = require('config');
 const express = require('express');
 const app = express();    //Convention to assign function to app varaible
-app.use(express.json());    //Adding a middleware so that request body is parsed in json object automatically
 const Joi = require('joi');
+const logger = require('./logger');
+const helmet = require('helmet');   //Add secure headers to request
+const morgan = require('morgan');   //Logs request
+app.use(express.json());    //Adding a middleware so that request body is parsed in json object automatically
+app.use(express.urlencoded({extended: true}));    //Parses the urlencoded form data to JSON format in request.body
+app.use(express.static('public'));    //Let us serve the static files listed in public directory.
+app.use(helmet());
+
+//Adding templating engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//Middlewares
+
+console.log(`Node environment: ${process.env.NODE_ENV}`);
+console.log(`Node environment: ${app.get('env')}`);
+
+if (app.get('env') === 'development')
+  app.use(morgan('tiny'));    //Use this middleware in development environment only
+
+app.use(logger);
+
+app.use(function(req, res, next){
+  console.log('Authenticating...');
+  next();
+});
+
+//Configuration
+
+console.log(`Applicantion Name: ${config.get('name')}`);
+console.log(`Mail Server Name: ${config.get('mail.host')}`);
+console.log(`Mail Server Password comming from environment variable: ${config.get('mail.password')}`);
+
+//Actual Logic
 
 let courses = [
   { id: 1, name: 'course1' },
@@ -10,7 +44,8 @@ let courses = [
 ];
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  // res.send('Hello World');
+  res.render('index', { title: 'My Express App', message: 'Hello World' });
 });
 
 app.get('/api/courses', (req, res) => {
